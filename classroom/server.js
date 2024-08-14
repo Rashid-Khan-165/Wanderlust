8aa5d0df4d14575e6474e5324907fe7775ae7dd4
Rashid-Khan-165 <williamsonkane7539@gmail.com>
2024-08-14 11:31:00 +0530

    Changes in cookies in server.js

--- classroom/server.js
+++ classroom/server.js
@@ -2,14 +2,32 @@ const express = require("express");
 const app = express();
 const users = require("./routes/users.js");
 const posts = require("./routes/posts.js");
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser("secretcode"));
+
+app.get("/getsignedcookies",(req,res)=>{
+    res.cookie("madeIn","India",{signed:true});
+    res.send("signed cookie sent")
+});
+
+app.get("/verify",(req,res)=>{
+    console.log(req.signedCookies);
+    res.send("verified");
+})
 
 app.get("/getcookies",(req,res)=>{
     res.cookie("greet","namaste");
-    res.cookie("madeIn","India");
     res.send("sent you some cookies!");
 })
 
+app.get("/greet",(req,res)=>{
+    let{name = "anonymous"} = req.cookies;
+    res.send(`Hi,${name}`);   
+})
+
 app.get("/",(req,res)=>{
+    console.dir(req.cookies);
     res.send("Hi,I am root!")  
 })
 
