b231eb9eba5898d55cf1448c0ca698d2e40aa53e
Rashid Khan <williamsonkane7539@gmail.com>
2024-08-15 13:08:54 +0530

    Add new folders and files

--- routes/listing.js
+++ routes/listing.js
@@ -12,7 +12,7 @@ const validateListing = (req,res,next)=>{
         let errMsg = error.details.map((el)=> el.message).join(",");
         throw new ExpressError (400,errMsg);
     }else{
-        next();
+        next();                                                    
     }
 }; 
 
@@ -32,6 +32,10 @@ router.get("/new",(req,res)=>{
 router.get("/:id",wrapAsync(async(req,res)=>{
     let {id}= req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
     res.render("./listings/show.ejs",{listing});
 }));
 
@@ -42,6 +46,7 @@ router.post("/",
     //let {title,description,image,price,country,location}=req.body;
     const newListing = new Listing(req.body.listing);
     await newListing.save();
+    req.flash("success","New listing Created!");
     res.redirect("/listings");
     })
 );
@@ -50,6 +55,10 @@ router.post("/",
 router.get("/:id/edit",wrapAsync(async(req,res)=>{
     let {id}= req.params;
     const listing = await Listing.findById(id);
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
     res.render("listings/edit.ejs",{listing});
 }));
 
@@ -59,6 +68,7 @@ router.put("/:id",
     wrapAsync(async(req,res)=>{
     let {id}= req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    req.flash("success","Listing Updated!");
     res.redirect(`/listings/${id}`);
 }));
 
@@ -67,6 +77,7 @@ router.delete("/:id",wrapAsync(async(req,res)=>{
     let {id}= req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash("success"," Listing Deleted!");
     res.redirect("/listings");
 }));
 
