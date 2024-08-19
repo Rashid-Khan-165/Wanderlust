03da691cb309539e75c065dc41d8e0629d15383b
Rashid Khan <williamsonkane7539@gmail.com>
2024-08-19 16:52:03 +0530

    I am working on Authentication by using Passport and creating Signup and Login routes.

--- classroom/server.js
+++ classroom/server.js
@@ -8,7 +8,7 @@ const path =require("path");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
- 
+  
 const sessionOptions = {
     secret : "mysupersecretstring",
     resave:false,
