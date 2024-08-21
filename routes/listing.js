d89341825db9f44325cb2e975306df583b6b226f
Rashid Khan <williamsonkane7539@gmail.com>
2024-08-21 13:34:52 +0530

    Adding Listing Owner

--- routes/listing.js
+++ routes/listing.js
@@ -33,11 +33,12 @@ router.get("/new",isLoggedIn,(req,res)=>{
 router.get("/:id",
     wrapAsync(async(req,res)=>{
     let {id}= req.params;
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id).populate("reviews").populate("owner");
     if(!listing){
         req.flash("error","Listing you requested for does not exist!");
         res.redirect("/listings");
     }
+    console.log(listing);
     res.render("./listings/show.ejs",{listing});
 }));
 
@@ -48,6 +49,7 @@ router.post("/",
     wrapAsync(async(req,res,next)=>{
     //let {title,description,image,price,country,location}=req.body;
     const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
     await newListing.save();
     req.flash("success","New listing Created!");
     res.redirect("/listings");
