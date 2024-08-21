ef86fdaa02b6233945c55e130f7175e2d5713d8a
Rashid Khan <williamsonkane7539@gmail.com>
2024-08-21 21:40:06 +0530

    Setting Authorization for Reviews by Creating Middleware named isReviewAuthor

--- routes/review.js
+++ routes/review.js
@@ -4,17 +4,19 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview} = require("../middleware.js");
+const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
 
 
 
 //Post - Review Route
 router.post("/",
+    isLoggedIn,
     validateReview,
     wrapAsync (async(req,res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
-  
+    newReview.author = req.user._id;
+    console.log(newReview);
     listing.reviews.push(newReview);
   
     await newReview.save();
@@ -24,7 +26,10 @@ router.post("/",
   }));
   
   //Delete Review Route
-  router.delete("/:reviewId",wrapAsync(async(req,res)=>{
+  router.delete("/:reviewId",
+    isLoggedIn,
+    isReviewAuthor,
+    wrapAsync(async(req,res)=>{
       let { id , reviewId} = req.params;
   
       await Listing.findByIdAndUpdate(id, {$pull : {reviews : reviewId}});
